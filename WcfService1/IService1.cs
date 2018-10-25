a12447be373535f48248dbd9f2f0347c5a58df62
sachinkale90 <sunnysachin15@gmail.com>
2018-10-24 19:11:20 -0700

    Initial commit

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {       
+        [OperationContract]
+        [WebInvoke(Method ="POST", UriTemplate ="/AddTrainee", RequestFormat =WebMessageFormat.Json, BodyStyle =WebMessageBodyStyle.Wrapped, ResponseFormat =WebMessageFormat.Json)]
+        string PostTrainee(Trainee sampleTrainee);
+
+        // TODO: Add your service operations here
+    }
+    
+    [DataContract]
+    public class Trainee
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Designation { get; set; }
+    }
+}
---
+++ WcfService1/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApplication2.Controllers
+{
+    using System.Web.Mvc;
+    using Newtonsoft.Json;
+    using ServiceReference1;
+    public class HomeController : Controller
+    {
+        private Service1Client client;
+        public HomeController()
+        {
+            client = new Service1Client();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public string Submit(string trainee)
+        {
+            var traineeInfo = JsonConvert.DeserializeObject<Trainee>(trainee);               
+            var name = client.PostTrainee(traineeInfo);
+            return name;
+        }
+    }
+}
